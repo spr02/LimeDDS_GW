d47d481af969fd2b275cbf4aa99bfd5c23f06dcc
Jannik Springer <jannik.springer@rwth-aachen,de>
2018-01-04 22:45:27 +0100

    Automated rbf file generation now working.

--- output_files/gen_prg_files.tcl
+++ output_files/gen_prg_files.tcl
@@ -3,7 +3,7 @@ set module [lindex $quartus(args) 0]
 if [string match "quartus_asm" $module] {
 	# Include commands here that are run after the assember
 	post_message "Running after assembler"
-	set cmd "quartus_cpf -c rbf_file_setup.cof"
+	set cmd "quartus_cpf -c output_files/rbf_file_setup.cof"
 	#qexec "quartus_cpf -c rbf_file_setup.cof"
 	# If the command can't be run, return an error.
 	if { [catch {open "|$cmd"} input] } {
