73e0932aa7b4e7ae7319bf7637ee7cb40ec0b326
Jannik Springer <jannik.springer@rwth-aachen,de>
2019-01-05 15:22:54 +0100

    Added CIC Decimator (submodule) in receive path.

--- src/dsp_radar/radar_dsp_top.vhd
+++ src/dsp_radar/radar_dsp_top.vhd
@@ -36,8 +36,13 @@ architecture arch of radar_dsp is
 	signal RxFifoVldxS, RxFifoRdyxS		: std_logic;
 	signal RxFifoIxD, RxFifoQxD			: std_logic_vector((IQ_WIDTH_IN - 1) downto 0);
 	
+	signal TxComplementQxD				: std_logic_vector((IQ_WIDTH_IN - 1) downto 0);
+	
 	signal DcVldxS, DcRdyxS				: std_logic;
 	signal DcIxD, DcQxD					: std_logic_vector((IQ_WIDTH_OUT - 1) downto 0);
+	
+	signal MulVldxS, MulRdyxS			: std_logic;
+	signal MulIxD, MulQxD				: std_logic_vector((IQ_WIDTH_OUT - 1) downto 0);
 begin
 
 	iqfifo0 : entity work.dds_iqfifo
@@ -88,11 +93,19 @@ begin
 --  		QxDO		=> QxDO
 	);
 	
+	p_comb_complement : process(TxQxDI)
+		variable v_complement : std_logic_vector((TxQxDI'length - 1) downto 0);
+	begin
+		v_complement := not TxQxDI;
+		TxComplementQxD <= std_logic_vector(signed(v_complement) + 1);
+	end process;
+	
 	cplx_mul0 : entity work.cplx_mul
 	generic map(
 		A_WIDTH		=> IQ_WIDTH_OUT,
 		B_WIDTH		=> IQ_WIDTH_IN,
-		P_WIDTH		=> IQ_WIDTH_OUT
+		P_WIDTH		=> IQ_WIDTH_OUT,
+		FSR_SHIFT	=> 0
 	)
 	port map(
 		ClkxCI		=> ClkxCI,
@@ -101,18 +114,66 @@ begin
 		-- rx data
 		AVldxSI		=> DcVldxS,
 		ARdyxSO		=> DcRdyxS,
+-- 		ArealxDI		=> std_logic_vector(to_signed(2, IQ_WIDTH_OUT)),
+-- 		AimagxDI		=> std_logic_vector(to_signed(0, IQ_WIDTH_OUT)),
 		ArealxDI	=> DcIxD,
 		AimagxDI	=> DcQxD,
 		-- tx data
 		BVldxSI		=> TxVldxSI,
 		BRdyxSO		=> TxRdyxSO,
-		BrealxDI	=> TxQxDI,
-		BimagxDI	=> TxIxDI,
+		BrealxDI	=> TxIxDI,
+		BimagxDI	=> TxComplementQxD,
 		-- out
-		PVldxSO		=> OutValxSO,
-		PRdyxSI		=> OutRdyxSI,
-		PrealxDO	=> IxDO,
-		PimagxDO	=> QxDO
+		PVldxSO		=> MulVldxS,
+		PRdyxSI		=> MulRdyxS,
+		PrealxDO	=> MulIxD,
+		PimagxDO	=> MulQxD
+-- 		PVldxSO		=> OutValxSO,
+-- 		PRdyxSI		=> OutRdyxSI,
+-- 		PrealxDO	=> IxDO,
+-- 		PimagxDO	=> QxDO
+	);
+	
+	cic_decim_i: entity work.cic_decim
+	generic map(
+		N	=> 4,
+		M	=> 1,
+		R	=> 64,
+		B	=> 16
+	)
+	port map(
+		ClkxCI		=> ClkxCI,
+		RstxRBI		=> RstxRBI,
+		ClkEnxSI	=> '1',
+		-- multiplier out (dechirped singal)
+		ValidxSI	=> MulVldxS,
+		ReadyxSO	=> MulRdyxS,
+		DataxDI		=> MulIxD,
+		-- decimator out
+		ValidxSO	=> OutValxSO,
+		ReadyxSI	=> OutRdyxSI,
+		DataxDO		=> IxDO
+	);
+	
+	cic_decim_q: entity work.cic_decim
+	generic map(
+		N	=> 4,
+		M	=> 1,
+		R	=> 64,
+		B	=> 16
+	)
+	port map(
+		ClkxCI		=> ClkxCI,
+		RstxRBI		=> RstxRBI,
+		ClkEnxSI	=> '1',
+		-- multiplier out (dechirped singal)
+		ValidxSI	=> MulVldxS,
+		ReadyxSO	=> open,
+		DataxDI		=> MulQxD,
+		-- decimator out
+		ValidxSO	=> open,
+		ReadyxSI	=> OutRdyxSI,
+		DataxDO		=> QxDO
 	);
 	
 	
